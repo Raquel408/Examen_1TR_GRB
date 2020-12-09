24f7004fd0cc89a499736e01681f4b0d2cb15941
Raquel408 <72872486+Raquel408@users.noreply.github.com>
2020-12-09 18:55:15 +0100

    Movimiento x y z
    Se puede mover en horizontal y vertical pero aun no esta reuelto la condidion de  no moverse hacia atras

---
+++ RaquelSanchezNavarro/Assets/Scripts/ViperMove.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ViperMove : MonoBehaviour
+
+{
+
+
+    public float speed;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        speed = 15f;
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        MovimientoViper();
+    }
+
+
+    void MovimientoViper()
+    {
+        float posX = transform.position.x;
+        float posZ = transform.position.z;
+        float desplZ = Input.GetAxis("Vertical");
+        float desplX = Input.GetAxis("Horizontal");
+
+         
+            transform.Translate(Vector3.right * Time.deltaTime * speed * desplX);
+         
+
+         
+            transform.Translate(Vector3.forward * Time.deltaTime * speed * desplZ);
+         
+
+
+    }
+}
+
+
