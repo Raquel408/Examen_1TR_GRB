60aca2db5cc327cbf83350c510fdcf99223ef937
Raquel408 <72872486+Raquel408@users.noreply.github.com>
2020-12-09 20:44:41 +0100

    .
    son scripts que se me olvidaron guardar

--- RaquelSanchezNavarro/Assets/Scripts/EnemyCreator.cs
+++ RaquelSanchezNavarro/Assets/Scripts/EnemyCreator.cs
@@ -4,6 +4,24 @@ using UnityEngine;
 
 public class EnemyCreator : MonoBehaviour
 {
+
+    //---SCRIPT ASOCIADO AL EMPTY OBJECT QUE CREARÁ LOS OBSTÁCULOS--//
+
+    //Variable que contendré el prefab con el obstáculo
+    [SerializeField] GameObject EnemySphere;
+
+    //Variable que tiene la posición del objeto de referencia
+    [SerializeField] Transform InitPos;
+
+    //Variables para generar columnas de forma random
+    private float randomNumber;
+    Vector3 RandomPos;
+
+    //Distancia a la que se crean las columnas iniciales
+    [SerializeField] float distanciaInicial = 3;
+    //Acceder a los componentes de la nave
+    public GameObject EnemySphere;
+    private SpaceshipMove spaceshipMove;
     // Start is called before the first frame update
     void Start()
     {
